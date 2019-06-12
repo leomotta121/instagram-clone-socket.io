160d73cbe4c633d9238d947d401699e7baa1c1f8
Leonardo Motta <leo@domboscoatacado.com.br>
2019-06-12 06:43:18 -0300

    Like posts

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,9 +1,35 @@
 const ApiError = require('../services/apiError');
 
+const sharp = require('sharp');
 const Post = require('../models/Post');
 
-exports.index = async (req, res, next) => {};
+exports.index = async (req, res, next) => {
+  const posts = await Post.find().sort('-createdAt');
+
+  return res.json(posts);
+};
 
 exports.store = async (req, res, next) => {
-  res.json({ ok: true });
+  const { author, place, description, hashtags } = req.body;
+  const { filename: image } = req.file;
+
+  const post = await Post.create({
+    author,
+    place,
+    description,
+    hashtags,
+    image
+  });
+
+  res.json(post);
+};
+
+exports.like = async (req, res, next) => {
+  const post = await Post.findById(req.params.id);
+
+  post.likes += 1;
+
+  await post.save();
+
+  return res.json(post);
 };
--- src/routes/v1/postRoutes.js
+++ src/routes/v1/postRoutes.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('../../services/upload');
 const router = express.Router();
 
 const isAuth = require('../../services/isAuth');
+const upload = multer(uploadConfig);
 
-const { index, store } = require('../../controllers/postController');
+const { index, store, like } = require('../../controllers/postController');
 
 // GET /v1/post/index
 router.get('/index', index);
 
 // POST /v1/post/store
-router.post('/store', store);
+router.post('/store', upload.single('image'), store);
+
+// POST /v1/post/like/:id
+router.post('/like/:id', like);
 
 module.exports = router;
