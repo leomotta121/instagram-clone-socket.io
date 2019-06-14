53a85ef60034fa944b8001c3bc6e64036a75387e
Leonardo Motta <leo@domboscoatacado.com.br>
2019-06-13 21:43:13 -0300

    Fix io set up

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ const ApiError = require('./services/apiError');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const logger = require('morgan');
 const path = require('path');
+const http = require('http');
 const v1 = require('./routes/v1');
-const io = require('./server');
 
 const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 app.use((req, res, next) => {
   req.io = io;
@@ -15,7 +18,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors);
+app.use(cors());
+
+app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -38,4 +43,4 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, status: status });
 });
 
-module.exports = app;
+module.exports = server;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,7 @@
 const { PORT, DB_USER, DB_PASSWORD, DB_NAME } = require('./services/envVariables');
 
-const app = require('./app');
-const http = require('http');
+const server = require('./app');
 const mongoose = require('mongoose');
-
-const server = http.createServer(app);
-const io = require('socket.io')(server);
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0-sud5s.mongodb.net/${DB_NAME}`, {
   useNewUrlParser: true,
@@ -16,5 +12,3 @@ mongoose.connection
   .on('error', error => console.warn('error: ' + error));
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
-
-module.exports = io;
